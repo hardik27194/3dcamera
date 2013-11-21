dcca9119418a3e48eb21d12fc39c1528cc1bc1b7
tianwalker2012 <sunnyskyunix@gmail.com>
2013-11-21 16:01:21 +0800

    Initial Commit

---
+++ FeatherCV/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FeatherCV
+//
+//  Created by xietian on 13-11-21.
+//  Copyright (c) 2013年 tiange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EZAppDelegate class]));
+    }
+}
---
+++ FeatherCVTests/FeatherCVTests.m
@@ -0,0 +1,34 @@
+//
+//  FeatherCVTests.m
+//  FeatherCVTests
+//
+//  Created by xietian on 13-11-21.
+//  Copyright (c) 2013年 tiange. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FeatherCVTests : XCTestCase
+
+@end
+
+@implementation FeatherCVTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
