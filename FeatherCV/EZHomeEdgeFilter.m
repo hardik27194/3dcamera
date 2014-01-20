b5832a1ca4fef8a9006373f9d60974dee1ab47de
tianwalker2012 <sunnyskyunix@gmail.com>
2014-01-20 19:07:32 +0800

    Change to original line

--- FeatherCV/EZHomeEdgeFilter.m
+++ FeatherCV/EZHomeEdgeFilter.m
@@ -34,6 +34,7 @@ NSString *const kGPUImageHomeThresholdEdgeDetectionFragmentShaderString = SHADER
  
  void main()
  {
+     /**
      float bottomLeftIntensity = texture2D(inputImageTexture, bottomLeftTextureCoordinate).r;
      float topRightIntensity = texture2D(inputImageTexture, topRightTextureCoordinate).r;
      float topLeftIntensity = texture2D(inputImageTexture, topLeftTextureCoordinate).r;
@@ -48,6 +49,21 @@ NSString *const kGPUImageHomeThresholdEdgeDetectionFragmentShaderString = SHADER
      float delta = mag - threshold;
      delta = abs(delta) * delta;
      mag = clamp(mag + delta,0.0, 1.0);
+     gl_FragColor = vec4(vec3(mag), 1.0);
+      **/
+     float bottomLeftIntensity = texture2D(inputImageTexture, bottomLeftTextureCoordinate).r;
+     float topRightIntensity = texture2D(inputImageTexture, topRightTextureCoordinate).r;
+     float topLeftIntensity = texture2D(inputImageTexture, topLeftTextureCoordinate).r;
+     float bottomRightIntensity = texture2D(inputImageTexture, bottomRightTextureCoordinate).r;
+     float leftIntensity = texture2D(inputImageTexture, leftTextureCoordinate).r;
+     float rightIntensity = texture2D(inputImageTexture, rightTextureCoordinate).r;
+     float bottomIntensity = texture2D(inputImageTexture, bottomTextureCoordinate).r;
+     float topIntensity = texture2D(inputImageTexture, topTextureCoordinate).r;
+     float h = -topLeftIntensity - 2.0 * topIntensity - topRightIntensity + bottomLeftIntensity + 2.0 * bottomIntensity + bottomRightIntensity;
+     float v = -bottomLeftIntensity - 2.0 * leftIntensity - topLeftIntensity + bottomRightIntensity + 2.0 * rightIntensity + topRightIntensity;
+     
+     float mag = length(vec2(h, v));
+     
      gl_FragColor = vec4(vec3(mag), 1.0);
  }
  );
