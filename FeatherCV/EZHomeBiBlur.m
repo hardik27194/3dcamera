7da62605ae625aa0601562bb8d7aa512f6277996
tianwalker2012 <sunnyskyunix@gmail.com>
2014-01-26 16:18:11 +0800

    Change the color ratio.

--- FeatherCV/EZHomeBiBlur.m
+++ FeatherCV/EZHomeBiBlur.m
@@ -22,7 +22,7 @@ NSString *const kGPUImageHomeBilateralFilterFragmentShaderString = SHADER_STRING
  
  lowp float sigmoid(highp float mixVal, highp float midVal)
  {
-     highp float mixedVal = -(mixVal - midVal) * 1.3;
+     highp float mixedVal = -(mixVal - midVal) * 1.0;
      return 1.0/(exp(mixedVal) + 1.0);
  }
  
